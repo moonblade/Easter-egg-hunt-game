369f88f810828c96727a7fda92f71dffb7207edc
Nisham <moonblade168@gmail.com>
2016-04-19 17:29:52 +0530

    app init

---
+++ app/app/components/dashboard/dashboardController.js
@@ -0,0 +1,11 @@
+angular
+    .module('altairApp')
+    .controller('dashboardCtrl', [
+        '$rootScope',
+        '$scope',
+        '$interval',
+        '$timeout',
+        'variables',
+        function ($rootScope,$scope,$interval,$timeout,variables) {
+
+        }])
